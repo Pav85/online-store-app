c4c21cac5db76e4e2f6b9941b346a628b8d37e62
Pawel <pawel.werbowy@gmail.com>
2024-05-27 14:07:24 +0100

    Update NavBar.js

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Online Store</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
+          <Nav.Link onClick={() => navigate("/products")}>Products</Nav.Link>
+          <Nav.Link onClick={() => navigate("/about")}>About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default NavBar;
