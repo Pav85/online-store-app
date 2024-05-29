b0dad5885e906cfb7c6baec556c6a1dddcc5b1b4
Pawel <pawel.werbowy@gmail.com>
2024-05-29 20:58:54 +0100

    fixed layout

--- src/components/totalPrice/TotalPrice.js
+++ src/components/totalPrice/TotalPrice.js
@@ -9,7 +9,7 @@ const TotalPrice = ({ total }) => {
         <Card.Title>
           <h2>Total Price</h2>
         </Card.Title>
-        <Card.Text className="mt-3 text-end me-3">
+        <Card.Text className="mt-3 text-end me-3 fs-4">
           £{total.toFixed(2)}
         </Card.Text>
       </Card.Body>
