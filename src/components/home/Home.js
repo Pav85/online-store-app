35fdec1fb9afece6398f88ca18c74f7f24242063
Pawel <pawel.werbowy@gmail.com>
2024-05-29 09:50:46 +0100

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const Home = () => {
+  const [name, setName] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setName("");
+  };
+
   return <div>Home</div>;
 };
 
