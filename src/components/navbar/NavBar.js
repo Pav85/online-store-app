d13c08d442093b6162d1ca013bb81c236a88a9f0
Pawel <pawel.werbowy@gmail.com>
2024-05-29 20:01:10 +0100

    styling

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/my-online-store-high-resolution-logo-black-transparent.png";
 
 const NavBar = () => {
   const navigate = useNavigate();
 
   return (
     <Navbar bg="light" expand="lg" className="p-1" fixed="top">
-      <Navbar.Brand href="/">Online Store</Navbar.Brand>
+      <Navbar.Brand href="/">
+        <img
+          src={logo}
+          width="auto"
+          height="50"
+          className="d-inline-block align-top"
+          alt="Online Store logo"
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto">
-          <Nav.Link onClick={() => navigate("/")}>Home</Nav.Link>
-          <Nav.Link onClick={() => navigate("/products")}>Products</Nav.Link>
-          <Nav.Link onClick={() => navigate("/about")}>About</Nav.Link>
+        <Nav className="ms-auto me-4">
+          <Nav.Link onClick={() => navigate("/")} className="me-3">
+            Home
+          </Nav.Link>
+          <Nav.Link onClick={() => navigate("/products")} className="me-3">
+            Products
+          </Nav.Link>
+          <Nav.Link onClick={() => navigate("/about")} className="me-3">
+            About
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
