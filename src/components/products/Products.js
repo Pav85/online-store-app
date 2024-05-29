b06194b18afdf907cdba69981c0686cbee3671a6
Pawel <pawel.werbowy@gmail.com>
2024-05-29 11:00:47 +0100

    totalprice component

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Card,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
 const Products = () => {
   return <div>Products</div>;
