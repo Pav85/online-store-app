e2a5a7c0346fa9ff6d89075bff8b811762400472
Pawel <pawel.werbowy@gmail.com>
2024-05-29 10:32:09 +0100

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const Home = () => {
   const [name, setName] = useState("");
@@ -14,7 +14,38 @@ const Home = () => {
     setName("");
   };
 
-  return <div>Home</div>;
+  return (
+    <Container className="mt-3">
+      <Row className="justify-content-center mb-3">
+        <Col md={4}>
+          {!loggedIn ? (
+            <Form>
+              <Form.Group controlId="formName">
+                <h4 className="text-center mb-3 main-heading">Login</h4>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Button
+                variant="primary"
+                onClick={handleLogin}
+                className="mt-3 w-100 text-center"
+              >
+                Login
+              </Button>
+            </Form>
+          ) : (
+            <div>
+              <h1>Hello</h1>
+            </div>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
